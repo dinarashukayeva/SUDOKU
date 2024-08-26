9efa018664e665228878b6ae30962f20034efa4c
dinarashukayeva <96359496+dinarashukayeva@users.noreply.github.com>
2024-08-26 11:26:20 -0600

    Update s.py

--- s.py
+++ s.py
@@ -1,1 +1,23 @@
+size = 9
+
+def isValid(grid, row, col, num):
+
+  #check for repeats in same row (horizontal)
+  for i in range(9):
+    if grid[row][i] == num:
+      return False
+
+  #check for repeats in column (vertical)
+  for j in range(9):
+    if grid[j][col] == num:
+      return False
+
+  #check for repeats in the same box
+  r = row - row%3
+  c = col - col%3
+  for i in range(3):
+    for j in range(3):
+      if grid[r+i][c+j]:
+        return False
+  return True
 
